9ae20f370b084dc74fc531cae40e1987ef90a974
Denis Slaschilin <denis.slaschilin@dev-pro.net>
2017-02-17 16:41:40 +0200

    Add new Redis helper functions

--- parser/storage.js
+++ parser/storage.js
@@ -9,16 +9,29 @@ const client = redis.createClient(config.redis.options);
 client.on('error', err => console.log('Error ' + err));
 
 function addToSet(name, value) {
-  return client.saddAsync(name, value).then(res => {
-    console.log(res);
-  });
+  return client.saddAsync(name, value);
 }
 
 function readSet(name) {
   return client.smembersAsync(name);
 }
 
+function expireSet(name) {
+  return client.expireAsync(name, config.redis.expires);
+}
+
+function deleteSet(name) {
+  return client.delAsync(name);
+}
+
+function isSetExists(name) {
+  return client.existsAsync(name);
+}
+
 module.exports = {
   addToSet,
-  readSet
+  readSet,
+  expireSet,
+  deleteSet,
+  isSetExists
 };
